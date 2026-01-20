d905713e8cdda8ee6393b92ce32e92a86848f15c
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2026-01-20 16:12:04 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,44 +7,45 @@ import fileRoutes from "./routes/file.routes.js";
 import folderRoutes from "./routes/folder.routes.js";
 
 dotenv.config();
+
 const app = express();
 
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",       
-      "https://cloud-file-storage-client-fl6n.vercel.app"  
+      "http://localhost:5173",
+      "https://cloud-file-storage-client-fl6n.vercel.app"
     ],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
+// REQUIRED for preflight
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp"
+    useTempFiles: false
   })
 );
-
-
-
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Cloud File Storage Server is running");
 });
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.use("/api/folders", folderRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
